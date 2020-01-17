50f20833256287015bdd02685a9ed1a9c91b56c8
ajini-sahaejana <49752003+ajini-sahaejana@users.noreply.github.com>
2020-01-17 16:28:31 +0530

    register.js updated

--- js/register.js
+++ js/register.js
@@ -1,3 +1,6 @@
+function red() {
+  location.replace("submit.html");
+}
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
