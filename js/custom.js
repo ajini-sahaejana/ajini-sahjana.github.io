bb03130dafaf1491a18fbca4d210cc360b193d6d
kosala98 <dayarathnekosala90@gmail.com>
2020-01-21 09:22:20 +0530

    Final updates

--- js/custom.js
+++ js/custom.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
 			});
 			$('.navbar-dark').css({
 				'background-color': 'rgba(20,20,20,0)'
-			})
+			});
 		}else{
 			$('.navbar').css({
 				'margiin-top':'0px',
@@ -20,7 +20,90 @@ $(document).ready(function () {
 			$('.navbar-dark').css({
 				'background-color': 'rgba(20,20,20,0.9)',
 				'border-color': '#444'
-			})
+			});
 		}
 	});
+});
+
+
+
+
+//add smooth scrolling
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 1000, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+
+/* active menu item on click */
+$(document).ready(function() {
+    
+    'use strict';
+    
+    
+    $('.navbar-nav li a').click(function(){
+        
+        'use strict';
+        
+        $('.navbar-nav li a').parent().removeClass("active");
+        
+        $(this).parent().addClass("active");
+    
+    });
+});
+
+
+// highlight menu item on scroll
+$(document).ready(function() {
+    
+    'use strict';
+    
+    $(window).scroll( function() {
+        
+        'use strict';
+        
+        $("section").each(function()  {
+            
+            'use strict';
+            
+            var bb = $(this).attr("id");  // ABOUT, CONTACT, DOWNLOAD
+            var hei = $(this).outerHeight();
+            var grttop = $(this).offset().top - 70;
+            
+            if ($(window).scrollTop() > grttop && $(window).scrollTop() < grttop + hei) {
+             
+                $(".navbar-nav li a[href='#" + bb + "']").parent().addClass("active");
+           
+            } else {
+                $(".navbar-nav li a[href='#" + bb + "']").parent().removeClass("active");
+                
+            }
+            
+            
+        });
+    
+    
+    });
+    
+    
 });
--- js/login.js
+++ js/login.js
@@ -12,3 +12,4 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
+
